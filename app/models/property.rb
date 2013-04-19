20540750ea5b6783cda79928d961caa29c756ae3
rubygeek <nola@rubygeek.com>
2013-04-19 10:31:45 -0500

    completed tdd exercise

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,7 @@
 class Property < ActiveRecord::Base
-  attr_accessible :city, :description, :name, :state, :zip, :phone
+  attr_accessible :city, :description, :name, :state, :zip, :phone, :management_company
   validates_presence_of :name, :city, :state, :zip
   validates_uniqueness_of :name
+  
+  belongs_to :management_company
 end
